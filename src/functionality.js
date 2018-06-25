d36ad2d62a3119ace47739ec96fe227e7469f383
dylanOshima <dylanoshima@gmail.com>
2018-06-25 23:21:54 +0800

    Adds basic sharding for shards=1. Breaks for more shards.

--- src/functionality.js
+++ src/functionality.js
@@ -1,12 +1,4 @@
-/**
- * Creates a pentagon at the given id with
- * @param  {[int]} x  [width of pentagon]
- * @param  {[int]} y  [height of pentagon]
- * @param  {[string]} id [id of SVG element to place pentagon]
- * @param  {[int]} scaleX [scales in the x dimension] optional
- * @param  {[int]} scaleY [scales in the y dimension] optional
- */
-const generatePentagon = function(x, y, id, num=0, scaleX=1, scaleY=1) {
+const generatePentagon = function(x, y, id, numShards=0, scaleX=1, scaleY=1) {
   var $svg = $(id);
 
   //1. Apply scales if they exist
@@ -26,7 +18,7 @@ const generatePentagon = function(x, y, id, num=0, scaleX=1, scaleY=1) {
   //Calculate center
   let center = [x/2, y/2];
 
-  // Testing
+  //Testing
   // let centerCircle = makeSVG('circle', {
   //   cx:center[0],
   //   cy:center[1],
@@ -51,16 +43,52 @@ const generatePentagon = function(x, y, id, num=0, scaleX=1, scaleY=1) {
 
   pentagon.attr('points', pointsPentagon)
 
-  // 5. Generate the individual shards shards pointsd on the {{num}}
-  console.log(points);
+  // 5. Generate the individual shards shards pointsd on the {numShards}
+  // Each shard uses the previous two points to calculate its' own
+  // points.
   for(let i=0;i<points.length;i++){
-    let j = (i+1) % (points.length),
-        pointSet = [points[i],points[j]],
+    let pointSet = [points[i],points[(i+1) % (points.length)]],
         base = distanceBetweenPoints(pointSet).toFixed(2),
-        height = distanceBetweenPoints([midPoint(pointSet), center]).toFixed(2);
-    console.log(i,"\nBase: ",base, " Height: ", height);
+        height = distanceBetweenPoints([midPoint(pointSet), center]).toFixed(2),
+        shards = [];
+
+    /**
+     * Generates the shards recursively.
+     * @param  {[type]} topPoints [description]
+     * @param  {[type]} n         [description]
+     * @return {[DOM Objects]}    returns a list of DOM objects
+     */
+    function generateShard(topPoints, n=numShards){
+      if(n === 1){
+        $svg.append(makeSVG('polygon', {
+          class: "shard",
+          fill:"#7CE0F9",
+          points: topPoints.concat([center]).map((point) => point.join(',')).join(' ')
+        }));
+      } else {
+        let nextPointSet = [[((2*n+1)*base)/(2*n),height/n],[base/(2*n), height/n]],
+            shardPoints  = topPoints.concat(nextPointSet);
+        console.log(i,": ",shardPoints);
+        $svg.append(makeSVG('polygon', {
+          class: "shard",
+          fill: "#7CE0F9",
+          points: shardPoints.map((point) => point.join(',')).join(' ')
+        }));
+        generateShard(nextPointSet, n-1);
+      }
+    }
+
+    generateShard(pointSet);
   }
 }
 
-// generatePentagon(500, 500, '#J-svg-pentagon',0, 1, 1);
-generatePentagon(500, 500, '#J-svg-pentagon',0, 1, 0.9);
+/**
+ * Creates a pentagon at the given id with
+ * @param  {[int]} x  [width of pentagon]
+ * @param  {[int]} y  [height of pentagon]
+ * @param  {[string]} id [id of SVG element to place pentagon]
+ * @param  {[int]} numShards [the number of shards per slice]
+ * @param  {[int]} scaleX [scales in the x dimension] optional
+ * @param  {[int]} scaleY [scales in the y dimension] optional
+ */
+generatePentagon(500, 500, '#J-svg-pentagon', 1, 1, 0.95);
---
+++ src/utility.js
@@ -0,0 +1,59 @@
+/**
+ * Checks that the points array has only two elements
+ * @param  {[Array[][]]} points A 2D array containing two sub arrays that hold: x, y
+ * @return {[bool]}        Returns true if points are only two
+ */
+
+function assertTwoPoints(points){
+  if(points.length !== 2){
+    throw("distanceBetweenPoints(): more than two points given");
+    return false;
+  } else if(points.length <= 1){
+    throw("distanceBetweenPoints(): less than two points given");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * Calculates the euclidean distance between two points
+ * @param  {[Array[][]]} points A 2D array containing two sub arrays that hold: x, y
+ * @return {[int]}        The distance between the two points
+ */
+
+function distanceBetweenPoints(points){
+  //console.log(points);
+  if(assertTwoPoints(points)){
+    a = points[0][0] - points[1][0];
+    b = points[0][1] - points[1][1];
+    return Math.sqrt(a*a + b*b);
+  }
+}
+
+/**
+ * Calculates the euclidean midPoint
+ * @param  {[Array[][]]} points A 2D array containing two sub arrays that hold: x, y
+ * @return {[Array]}        That hold the x and y values respectively
+ */
+
+function midPoint(points){
+  if(assertTwoPoints(points)){
+    a = (points[0][0] + points[1][0])/2;
+    b = (points[0][1] + points[1][1])/2;
+    return [a,b];
+  }
+}
+
+/**
+ * Makes an svg of a specific tag. See: https://stackoverflow.com/questions/3642035/jquerys-append-not-working-with-svg-element
+ * @param  {[String]} tag   The type of svg element (e.g. 'circle', 'polygon', etc.)
+ * @param  {[Object]} attrs An object containing pairs of each attribute and its' value
+ * @return {[DOM Element]}  A DOM element that can be injected
+ */
+function makeSVG(tag, attrs) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for(var k in attrs)
+        el.setAttribute(k, attrs[k]);
+    return el;
+}
