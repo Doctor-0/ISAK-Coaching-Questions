5459c560dddbe238a785323afedaeb76e65881d0
dylanOshima <dylanoshima@gmail.com>
2018-06-25 18:06:58 +0800

    Updated README to show how to launch the site

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Pentagon Diagram for ISAK
 This is a diagram to help students show how far they've progressed in five different areas: awareness, creativity, self-discipline, action-taking, and connecting.
 
+## How to run:
+*Git needs to be [installed](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)*
+1. Clone the [repository](https://help.github.com/articles/cloning-a-repository/)
+2. Open index.html in the browser of your choice
+
 ## Key Features:
 1. Should be embeddable on a Wix site using an <iframe>
 2. Should be able to reference how much of a section was selected
--- src/functionality.js
+++ src/functionality.js
@@ -1,21 +1,4 @@
 /**
- * [distanceBetweenPoints description]
- * @param  {[Arrray[][]]} points [A 2D array containing two sub arrays]
- * @return {[int]}        [The distance between the two points]
- */
-
-function distanceBetweenPoints(points){
-  if(points.length > 2){
-    console.log("distanceBetweenPoints() error: more then two points given");
-    return -1;
-  } else {
-    a = points[0][0] - points[1][0];
-    b = points[0][1] - points[1][1];
-    return Math.sqrt(a*a + b*b);
-  }
-}
-
-/**
  * Creates a pentagon at the given id with
  * @param  {[int]} x  [width of pentagon]
  * @param  {[int]} y  [height of pentagon]
@@ -24,51 +7,60 @@ function distanceBetweenPoints(points){
  * @param  {[int]} scaleY [scales in the y dimension] optional
  */
 const generatePentagon = function(x, y, id, num=0, scaleX=1, scaleY=1) {
-  var svg = $(id);
+  var $svg = $(id);
 
-  // 1. Points follow clockwise around the pentagon,
+  //1. Apply scales if they exist
+  if(scaleX !== 1 || scaleY !== 1){
+    x *= scaleX;
+    y *= scaleY;
+  }
+
+  // 2. Points follow clockwise around the pentagon,
   // starting from the top most point
-  let base = [[x/2, 0], //Top most point
+  var points = [[x/2, 0], //Top most point
               [x, y/2],
               [(3*x)/4, y],
               [x/4, y],
               [0, y/2]];
 
-  //2. Apply scales if they exist
-  if(scaleX !== 1 || scaleY !== 1){
-    base.forEach((point) => point[0] *= scaleX);
-    base.forEach((point) => point[1] *= scaleY);
-  }
-
+  //Calculate center
   let center = [x/2, y/2];
 
-  //3. Set width and height of frame based on points
-  svg.attr('width', base[1][0]); //base[1][0] is the right-most point
-  svg.attr('height', base[2][1]) //base[2][1] || base[3][1] is the bottom-most point
+  // Testing
+  // let centerCircle = makeSVG('circle', {
+  //   cx:center[0],
+  //   cy:center[1],
+  //   r:"10",
+  //   fill:"FFF"
+  // })
+  // $(id).append(centerCircle);
+
+  //3. Set width and height of frame pointsd on points
+  $svg.attr({
+    width: points[1][0], //points[1][0] is the right-most point
+    height: points[2][1] //points[2][1] || points[3][1] is the bottom-most point
+  });
 
   //4. Generate the pentagon
-  let pointsPentagon = "",
-      pentagon = svg.find('.pentagon');
+  let pointsPentagon = '',
+      pentagon = $svg.find('.pentagon');
 
-  for(let i=0;i<base.length;i++){
-    pointsPentagon += base[i].join(",") + " ";
+  for(let i=0;i<points.length;i++){
+    pointsPentagon += points[i].join(",") + " ";
   }
 
   pentagon.attr('points', pointsPentagon)
 
-  /**
-   * Creates base.length-1 shards for each triangle
-   * @param {[int]} num [the number of levels per shard]
-   * @return {[type]} [description]
-   */
-  function createShards() {
-    for(let i=0;i<base.length-1;i++){
-      let b = distanceBetweenPoints(base.slice(i,(i+2)%base.length));
-
-    }
+  // 5. Generate the individual shards shards pointsd on the {{num}}
+  console.log(points);
+  for(let i=0;i<points.length;i++){
+    let j = (i+1) % (points.length),
+        pointSet = [points[i],points[j]],
+        base = distanceBetweenPoints(pointSet).toFixed(2),
+        height = distanceBetweenPoints([midPoint(pointSet), center]).toFixed(2);
+    console.log(i,"\nBase: ",base, " Height: ", height);
   }
-
-  createShards()
 }
 
-generatePentagon(500, 500, '#J-svg-pentagon',0, 1, 0.90);
+// generatePentagon(500, 500, '#J-svg-pentagon',0, 1, 1);
+generatePentagon(500, 500, '#J-svg-pentagon',0, 1, 0.9);
