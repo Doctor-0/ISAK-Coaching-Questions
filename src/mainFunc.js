0d42fde91c8d4d8db3950beb0a90285bb826d5bf
dylanOshima <dylanoshima@gmail.com>
2018-08-10 01:51:48 +0800

    Adds the hovering of specific shard depending on skill

--- src/mainFunc.js
+++ src/mainFunc.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  const id = '#svg-pentagon';
+  const id = '#svg-pentagon',
+        numLevels = 8;
   //Sets the initial hash
   if(window.location.hash == ""){
     window.location.hash = '0,0&0,0&0,0&0,0&0,0';
   }
 
-  let points = generatePentagon(425, 500, id, 8, 1.05, 0.85, getHash(window.location.hash));
+  let points = generatePentagon(425, 500, id, numLevels, 1.05, 0.85, getHash(window.location.hash));
 
   //3. Set width and height of frame point on points
   $("#J-svg-pentagon").attr({
@@ -13,53 +14,78 @@ $(document).ready(function() {
     height: points[2][1] //points[2][1] || points[3][1] is the bottom-most point
   });
 
-  //Sets the popup window with 'coaching questions' when clicked
+
+  // Sets the popup window with 'coaching questions' when clicked
   function onClick(e) {
     console.log("teehee");
     let $popup = document.getElementById('popup-' + e.target.classList[1]);
     $popup.classList.toggle('hide');
   }
-  $(".awareness").click(onClick);
-  $(".connecting").click(onClick);
-  $(".creative").click(onClick);
-  $(".action").click(onClick);
-  $(".disciplined").click(onClick);
-
 
   // Sets the hovering effect
+  const opacity=0.15;
+
+  function updateShardsCascade(shard_id, val=1) {
+    for(let i=shard_id[1];i<=numLevels;i++){
+      $('.shard-' + shard_id[0] + i).attr('opacity', val);
+    }
+  }
+
   // Awareness
+  $(".awareness").click(onClick);
   $(".awareness").hover(function(e) {
-     e.target.classList.toggle('hover');
+     updateShardsCascade([0,0], opacity); //Connecting
+     updateShardsCascade([2,0], opacity); //Self-Disciplined
+     updateShardsCascade([3,0], opacity); //Creative
   }, function(e) {
-    e.target.classList.toggle('hover');
+    updateShardsCascade([0,0]); //Action-Taking
+    updateShardsCascade([2,0]); //Awareness
+    updateShardsCascade([3,0]); //Awareness
   });
 
   // Connecting
-
-  // $(".connecting").hover(onEnter, onLeave);
-  // $(".connecting").click(onEnter, onLeave);
-  //
-  // function onEnter(e) {
-  //   let shard_id = getID(e.target);
-  //   updateShardsCascade(shard_id, HIGH_LIGHT_COLOR);
-  // }
-  //
-  // function onLeave(e) {
-  //   let shard_id = getID(e.target),
-  //       currHash = getHash(),
-  //       cap = currHash[shard_id[0]][1];
-  //   updateShardsCascade(shard_id, NORMAL_COLOR, cap);
-  // }
-  //
-  // function onClick(e) {
-  //   let shard_id = getID(e.target);
-  //   setHash(shard_id);
-  // }
+  $(".connecting").click(onClick);
+  $(".connecting").hover(function(e) {
+     updateShardsCascade([2,0], opacity); //Self-Disciplined
+     updateShardsCascade([3,0], opacity); //Creative
+     updateShardsCascade([4,0], opacity); //Awareness
+  }, function(e) {
+    updateShardsCascade([2,0]); //Self-Disciplined
+    updateShardsCascade([3,0]); //Creative
+    updateShardsCascade([4,0]); //Awareness
+  });
 
   // Creative
-
+  $(".creative").click(onClick);
+  $(".creative").hover(function(e) {
+     updateShardsCascade([0,0], opacity); //Connecting
+     updateShardsCascade([4,0], opacity); //Awareness
+  }, function(e) {
+    updateShardsCascade([0,0]); //Action-Taking
+    updateShardsCascade([4,0]); //Awareness
+  });
 
   // Action
+  $(".action").click(onClick);
+  $(".action").hover(function(e) {
+     updateShardsCascade([0,0], opacity); //Connecting
+     updateShardsCascade([3,0], opacity); //Creative
+     updateShardsCascade([4,0], opacity); //Awareness
+  }, function(e) {
+    updateShardsCascade([0,0]); //Connecting
+    updateShardsCascade([3,0]); //Creative
+    updateShardsCascade([4,0]); //Awareness
+  });
 
   // Disciplined
+  $(".disciplined").click(onClick);
+  $(".disciplined").hover(function(e) {
+     updateShardsCascade([0,0], opacity); //Connecting
+     updateShardsCascade([1,0], opacity); //Action-Taking
+     updateShardsCascade([3,0], opacity); //Creative
+  }, function(e) {
+    updateShardsCascade([0,0]); //Connecting
+    updateShardsCascade([1,0]); //Action-Taking
+    updateShardsCascade([3,0]); //Creative
+  });
 })
